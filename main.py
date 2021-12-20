c144a70a8530044ee1101227784e161ad61db3ac
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2021-12-20 13:47:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')   
+
+#the sum of the two numbers variable
+sum = float(num1) + float(num2)
+sum2 = float(num1) - float(num2)
+sum3 = float(num1) * float(num2)
+sum4 = float(num1) / float(num2)
+
+#what operator to use
+choice = input('Enter an operator, + = addition, - = subtraction, * = multiplication and / = division: ')
+#different sums based on the operators
+if choice == '+': 
+  print('The sum of {0} and {1} is {2}'.format  (num1, num2, sum))
+
+elif choice == '-':
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum2))
+
+elif choice == '*':
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum3))a
+
+elif choice == '/':
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum4))
+
+else:
+      print("Invalid input!")
+
+   
